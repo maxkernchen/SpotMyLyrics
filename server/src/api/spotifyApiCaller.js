348454c0fb72924f74a3937ac9e216e8f10bee87
maxkernchen <max.f.kernchen@gmail.com>
2022-05-31 17:48:34 -0400

    added logic to renew spotify api token every 59 minutes

--- server/src/api/spotifyApiCaller.js
+++ server/src/api/spotifyApiCaller.js
@@ -1,21 +1,24 @@
 import SpotifyWebApi from 'spotify-web-api-node';
 import {config} from '../config/config.js';
 
-// TODO Replace with config file
 const spotifyApi = new SpotifyWebApi({
     clientId: config.spotifyclientid,
     clientSecret: config.spotifyclientsecret
   });
-// TODO make sure we get new token every 30 mins or so.
-export function initalizeSpotifyApi() {
 
-    spotifyApi.clientCredentialsGrant().then(
+var accessDate;
+
+// TODO make sure we get new token every 30 mins or so.
+ export  function initalizeSpotifyApi() {
+
+     spotifyApi.clientCredentialsGrant().then(
         function(data) {
           console.log('The access token expires in ' + data.body['expires_in']);
           console.log('The access token is ' + data.body['access_token']);
       
           // Save the access token so that it's used in future calls
           spotifyApi.setAccessToken(data.body['access_token']);
+          accessDate = new Date();
         },
         function(err) {
           console.log(
@@ -26,7 +29,15 @@ export function initalizeSpotifyApi() {
       );
 }
 
+
 export function getCurrentApiObj() {
+    let now = new Date();
+    let expiresDate = new Date(accessDate.getTime() + config.spotifyexpiresin);
+    if(now >= expiresDate){
+      console.log('Renewing access token Spotify API');
+      initalizeSpotifyApi();
+
+    }
     return spotifyApi;
     
 }
